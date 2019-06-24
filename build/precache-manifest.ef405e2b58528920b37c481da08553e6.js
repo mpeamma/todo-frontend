0c7c3ced8fb73970112d54315765cdb2429fba99
Michael Eamma <michael.eamma@credera.com>
2019-06-24 10:17:12 -0500

    Add build folder for testing

---
+++ build/precache-manifest.ef405e2b58528920b37c481da08553e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61129fa5963582426a708c1b0e9e7eda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8977536c0c38cd4935b3",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c195b1452c5d858df16a",
+    "url": "/static/js/2.a7dbb3cf.chunk.js"
+  },
+  {
+    "revision": "8977536c0c38cd4935b3",
+    "url": "/static/js/main.2287ff63.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
